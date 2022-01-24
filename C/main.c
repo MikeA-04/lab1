382acd6b9514a6a4323ca1ec1f8d320eecb9c66b
Mike Apreza <69696262+MikeA-04@users.noreply.github.com>
2022-01-24 10:39:43 -0600

    Add changes

--- C/main.c
+++ C/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern int extern_global_var; // TODO: Check symbol type in object file
-int global_var = 6; // TODO: Check symbol type in object file
+extern int extern_global_var; // Symbol type in object file: Notype
+int global_var = 6; // Symbol type in object file: Object
+static int static_global = 4; // Created static global, Object in .o file
 
 int main(){
   int factor = 2;
+  static int static_local = 9; // Created static local, Object in .o
+                              // Has .2836 under Name
   printf("%d, %d, %d\n", global_var, extern_global_var, factor);
   printf("Before modification:- secret value: %d\n", get());
-  multiply(factor);
+  multiply(factor); // External functions are UND
   printf("After modification:- secret value: %d\n", get());
 
 	return 0;
--- C/shared.c
+++ C/shared.c
@@ -1,6 +1,8 @@
-int extern_global_var = 5; // TODO: Check symbol type in object file
-
+int extern_global_var = 5; // Symbol type in object file: Object
+static int static_global = 99; // Created static global, Object in .o file
+                              // Has .1915 under the Name
 void multiply(int mfactor) {
+  static int static_local = 3; // Created static local, Object in .o
   extern_global_var = extern_global_var * mfactor;
 }
 
